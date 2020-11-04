66a70ccff78c0eeef46a33274a792cd4f263e3d1
charansoneji <charan.soneji.cls@gmail.com>
2020-11-05 00:32:05 +0400

    cards and pictures created with a bit of styling on home page

---
+++ portfolio/src/components/CardInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CardInfo(props){
+
+    return(
+        <p>CardInfo</p>
+    );
+}
+
+export default CardInfo;
--- portfolio/src/components/Carousel.js
+++ portfolio/src/components/Carousel.js
@@ -1,42 +1,78 @@
 import React from 'react';
 
+import medium from '../assets/images/medium.png';
+import myself from '../assets/images/myself.png';
+import github from '../assets/images/github.png';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Card from '../components/Card';
+
 class Carousel extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            items:[
+            items: [
                 {
                     id: 0,
-                    title: 'Dev Grub',
-                    subTitle: 'The Cookbook for Developers',
-                    imgSrc: devgrub,
-                    link: 'https://devgrub.com',
+                    title: 'Medium.com',
+                    subTitle: 'ML Blogger',
+                    imgSrc: medium,
+                    link: 'https://www.medium.com/100daysofml',
                     seletced: false
                 },
                 {
                     id: 1,
                     title: 'Charan Soneji',
-                    subTitle: 'ML Blogger',
-                    imgSrc: blogger,
-                    link: 'https://www.medium.com/100daysofml',
+                    subTitle: 'ML/DL Enthusiast',
+                    imgSrc: myself,
+                    link: '/',
                     seletced: false
                 },
                 {
                     id: 2,
-                    title: 'Everest',
-                    subTitle: 'A social Network for developers',
-                    imgSrc: devgrub,
-                    link: 'https://devgrub.com',
+                    title: 'Github',
+                    subTitle: 'Data Analyst and Developer',
+                    imgSrc: github,
+                    link: 'https://www.github.com/charansoneji',
                     seletced: false
                 }
             ]
         }
     }
 
+    handleCardClick = (id, card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+        items.forEach(item => {
+            if (item.id != id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return (
-            <p>Carousel Works</p>
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
         );
     }
 }
